e3a36664236d0cb4c1cd12d8d35f8119d5f74a31
chenqinhui <635520704@qq.com>
2017-10-10 22:53:30 +0800

    Revert "dishes details"

--- src/main/java/com/quickcanteen/model/OrderDishesKey.java
+++ src/main/java/com/quickcanteen/model/OrderDishesKey.java
@@ -1,17 +1,23 @@
 package com.quickcanteen.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
 public class OrderDishesKey {
     private Integer orderId;
 
     private Integer dishesId;
 
-    public OrderDishesKey(Integer orderId,Integer dishesId){
-        this.dishesId=dishesId;
-        this.orderId=orderId;
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public Integer getDishesId() {
+        return dishesId;
+    }
+
+    public void setDishesId(Integer dishesId) {
+        this.dishesId = dishesId;
     }
 }
